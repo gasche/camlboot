8d9d976bb7828b4b0981dcbc6aff408c480073d9
Gabriel Scherer <gabriel.scherer@gmail.com>
2021-03-03 11:15:26 +0100

    miniml/interp: dummy Printexc module

---
+++ miniml/interp/printexc.ml
@@ -0,0 +1,21 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*                                                                        *)
+(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* a dummy implementation of some Printexc functions used in lexgen.ml *)
+type raw_backtrace = unit
+
+let get_raw_backtrace () = ()
+
+let raise_with_backtrace exn () = raise exn
